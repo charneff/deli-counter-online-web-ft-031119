2595922f946e7e28b61dd7815bcb8c481f46ee61
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-11 23:41:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
   end
   else
     current_line = "The line is currently:"
-    deli.each.with_index(1)
+    deli.each.with_index(1) do |
     
 def take_a_number(deli, name)
   deli << name
