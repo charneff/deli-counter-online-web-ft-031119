5827f8a790aa555349aad25018af1bd2b746691f
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-11 23:35:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,5 @@
 def take_a_number(deli, name)
   deli << name
-  puts "Welcome, #{name}. You are number #{deli.length} in line."# Write your code here.
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+  # Write your code here.
