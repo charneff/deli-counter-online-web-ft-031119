ddf9dea66f60810862e8f31d22706bb81062897d
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-11 23:36:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,9 @@
 def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  end
   
+    
 def take_a_number(deli, name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.length} in line."
